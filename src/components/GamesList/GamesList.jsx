2ff485c3967ce22137e0946404dca336292aed19
Eo Soo Kim <eosookim@gmail.com>
2024-07-02 17:56:17 -0400

    add api calls to get data for gamelist

--- src/components/GamesList/GamesList.jsx
+++ src/components/GamesList/GamesList.jsx
@@ -1,8 +1,30 @@
 import './GamesList.scss'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function GamesList(){
+function GamesList({commonGenres}){
+    const [popularList, setPopularList] = useState([{game_id: ""}]);
+    const [gameList, setGameList] = useState([]);
+    useEffect (() => {
+        const getGenreGames = async () => {
+            const data = await axios.post(`http://localhost:8080/games`);
+            setPopularList(data.data);
+            console.log(data.data);
+        }
+        getGenreGames();
+    }, [])
+    useEffect (() => {
+        const getPopularData = async () => {
+            const data = await axios.post(`http://localhost:8080/games/popular`);
+            setPopularList(data.data);
+            console.log(data.data);
+        }
+        getPopularData();
+    }, [])
     return (
-        <></>
+        <>
+            <div>{popularList[0].game_id}</div>
+        </>
     )
 }
 
--- src/components/RelatedSearch/RelatedSearch.jsx
+++ src/components/RelatedSearch/RelatedSearch.jsx
@@ -1,6 +1,6 @@
 import './RelatedSearch.scss';
 
-export default function RelatedSearch({game, setGameTitle, selectGame, gameCount}){
+export default function RelatedSearch({game, selectGame, gameCount}){
     console.log(game);
     return (
         <button onClick={() => selectGame(game, gameCount)}>
