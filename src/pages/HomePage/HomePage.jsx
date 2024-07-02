9bb3c271744e9c9e26d768c686615225855c4676
Eo Soo Kim <eosookim@gmail.com>
2024-07-02 16:54:33 -0400

    creat recommendation page, stores most common genre from all inputs

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,98 @@
-import './HomePage.scss'
-import GamesForm from '../../components/GamesForm/GamesForm';
+import "./HomePage.scss";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import GamesForm from "../../components/GamesForm/GamesForm";
+import RelatedSearch from "../../components/RelatedSearch/RelatedSearch";
 
-function HomePage(){
-    return (
-        <main className="main">
-            <GamesForm />
-        </main>
-    )
+function HomePage() {
+  const [gameList, setGameList] = useState([]);
+  const [favGameOne, setFavGameOne] = useState("");
+  const [favGameTwo, setFavGameTwo] = useState("");
+  const [favGameThree, setFavGameThree] = useState("");
+  const [gameCount, setGameCount] = useState(1);
+  const [gameTitle, setGameTitle] = useState({ name: "" });
+  const navigate = useNavigate();
+
+  const toRecommendationPage = () => {
+    navigate("/recommendations", {
+      state: { games: [favGameOne, favGameTwo, favGameThree], count: gameCount - 1},
+    });
+  };
+
+  useEffect(() => {
+    const getGameList = async () => {
+      const gameListData = await axios.post(
+        `http://localhost:8080/games`,
+        gameTitle
+      );
+      let list = [];
+      gameListData.data.forEach((game) => {
+        list.push(game);
+      });
+      setGameList(list);
+    };
+    getGameList();
+  }, [gameTitle]);
+
+  function selectGame(game, count) {
+    if (count == 1) {
+      setFavGameOne(game);
+      console.log(game);
+    } else if (count == 2) {
+      setFavGameTwo(game);
+      console.log(game);
+    } else {
+      setFavGameThree(game);
+      console.log(game);
+    }
+    setGameList([]);
+    setGameCount(gameCount + 1);
+  }
+  return (
+    <main className="main">
+      <div className="games-form">
+        <h2 className="games-form__header"></h2>
+        <GamesForm
+          setGameTitle={setGameTitle}
+          title={favGameOne}
+          formNumber={1}
+          gameCount={gameCount}
+        />
+        <GamesForm
+          setGameTitle={setGameTitle}
+          title={favGameTwo}
+          formNumber={2}
+          gameCount={gameCount}
+        />
+        <GamesForm
+          setGameTitle={setGameTitle}
+          title={favGameThree}
+          formNumber={3}
+          gameCount={gameCount}
+        />
+      </div>
+      <div className="related-search__list">
+        {gameList.map((game) => {
+          return (
+            <RelatedSearch
+              game={game}
+              setGameTitle={setGameTitle}
+              selectGame={selectGame}
+              gameCount={gameCount}
+            />
+          );
+        })}
+      </div>
+      <button
+        className="recommend-button"
+        onClick={toRecommendationPage}
+        disabled={favGameOne == "" ? true : false}
+      >
+        Recommend
+      </button>
+    </main>
+  );
 }
 
 export default HomePage;
--- src/pages/RecommendationPage/RecommendationPage.jsx
+++ src/pages/RecommendationPage/RecommendationPage.jsx
@@ -1,12 +1,64 @@
-import './RecommendationPage.scss'
-import GamesList from '../../components/GamesList/GamesList';
+import "./RecommendationPage.scss";
+import GamesList from "../../components/GamesList/GamesList";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function RecommendationPage(){
-    return (
-        <>
-            <GamesList />
-        </>
-    )
+function RecommendationPage() {
+  const location = useLocation();
+  const [genreList, setGenreList] = useState([]);
+  const [commonGenres, setCommonGenres] = useState([]);
+  const [mostCommonGenres, setMostCommonGenres] = useState([]);
+  const { games, count } = location.state;
+
+  useEffect(() => {
+    console.log(location.state);
+    if (count == 1) {
+      setCommonGenres(games[0].genres);
+      console.log(games[0].genres);
+    } else if (count == 2) {
+      setGenreList(games[0].genres);
+      setCommonGenres([]);
+      games[1].genres.forEach((genre) => {
+        if (games[0].genres.includes(genre)) {
+          setCommonGenres((genreList) => [...genreList, genre]);
+        }
+      });
+    } else {
+      setCommonGenres([]);
+      setMostCommonGenres([]);
+      console.log(commonGenres);
+      games[2].genres.forEach((genre) => {
+        if (
+          games[0].genres.includes(genre) &&
+          games[1].genres.includes(genre)
+        ) {
+          setMostCommonGenres((genreList) => [...genreList, genre]);
+        } else if (games[0].genres.includes(genre)) {
+          setCommonGenres((genreList) => [...genreList, genre]);
+        } else if (games[1].genres.includes(genre)) {
+          setCommonGenres((genreList) => [...genreList, genre]);
+        }
+      });
+    }
+  }, []);
+  return (
+    <>
+      {mostCommonGenres.length > 0
+        ? mostCommonGenres
+        : commonGenres.length > 0
+        ? commonGenres
+        : genreList}
+      <GamesList
+        commonGenres={
+          mostCommonGenres.length > 0
+            ? mostCommonGenres
+            : commonGenres.length > 0
+            ? commonGenres
+            : genreList
+        }
+      />
+    </>
+  );
 }
 
 export default RecommendationPage;
