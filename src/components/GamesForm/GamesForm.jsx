fb5179a8bd824b99be46952ea70da4e0d2b92df5
Eo Soo Kim <eosookim@gmail.com>
2024-07-02 16:53:33 -0400

    move api calls to page instead of inside component

--- src/components/GamesForm/GamesForm.jsx
+++ src/components/GamesForm/GamesForm.jsx
@@ -1,61 +1,20 @@
 import "./GamesForm.scss";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Combobox from "react-widgets/Combobox";
 
-function GamesForm() {
-  const [gameList, setGameList] = useState([]);
-  const [favGameOne, setFavGameOne] = useState("");
-  const [favGameTwo, setFavGameTwo] = useState("");
-  const [favGameThree, setFavGameThree] = useState("");
-  const [gameTitle, setGameTitle] = useState({
-    name: "",
-  });
-
-  useEffect(() => {
-    const getGameList = async () => {
-      const gameListData = await axios.post(
-        `http://localhost:8080/games`,
-        gameTitle
-      );
-      let list = [];
-      gameListData.data.forEach((game) => {
-        console.log(game);
-        list.push(game.name);
-      });
-      setGameList(list);
-    };
-    getGameList();
-  }, [gameTitle]);
-  useEffect(() => {
-    console.log(favGameOne);
-  }, [favGameOne]);
+export default function GamesForm({ setGameTitle, title, formNumber, gameCount }) {
   return (
-    <div className="games-form">
-      <h2 className="games-form__header">Header</h2>
-      <form className="games-form__form">
-        <Combobox
-          className="games-form__combobox"
-          data={gameList}
-          onChange={(value) => setGameTitle({ name: value })}
-          onSelect={(value) => setFavGameOne(value)}
-          placeholder="Enter your favourite game"
-        />
-        <Combobox
-          hideCaret
-          hideEmptyPopup
-          data={["Red", "Yellow", "Blue", "Orange"]}
-          placeholder="Search for a color"
-          
-        />
-        <div className="games-form__buttons">
-          <button className="games-form__submit" type="submit">
-            SUBMIT
-          </button>
-        </div>
-      </form>
-    </div>
+    <form className="games-form__form">
+      <input
+        type="text"
+        className="games-form__input"
+        disabled={(formNumber == gameCount) ? false : true}
+        onChange={(e) => setGameTitle({ name: e.target.value })}
+        placeholder="Enter your favourite game"
+      ></input>
+      <div className="games-form__buttons">
+        <button className="games-form__submit" type="submit">
+          SUBMIT
+        </button>
+      </div>
+    </form>
   );
 }
-
-export default GamesForm;
